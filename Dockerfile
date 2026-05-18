2a939aefd8469fac3b354e3a7fcb433f10dacc27
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-05-18 20:57:49 -0300

    feat: Add Docker support to project

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# build
+FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
+WORKDIR /src
+
+COPY SteamCord.slnx .
+COPY SteamCord.Bot/ SteamCord.Bot/
+COPY SteamCord.Application/ SteamCord.Application/
+COPY SteamCord.Infrastructure/ SteamCord.Infrastructure/
+
+RUN dotnet restore SteamCord.Bot/SteamCord.Bot.csproj
+
+RUN dotnet publish \
+    SteamCord.Bot/SteamCord.Bot.csproj \
+    -c Release \
+    -o /app/publish \
+    --no-restore
+
+# runtime
+FROM mcr.microsoft.com/dotnet/aspnet:10.0
+WORKDIR /app
+
+COPY --from=build /app/publish .
+
+ENTRYPOINT ["dotnet", "SteamCord.Bot.dll"]
--- SteamCord.Application/ApplicationExtensions.cs
+++ SteamCord.Application/ApplicationExtensions.cs
@@ -1,3 +1,4 @@
+using DotEnv.Core;
 using Microsoft.Extensions.DependencyInjection;
 using SteamCord.Application.Configuration;
 
@@ -7,10 +8,21 @@ public static class ApplicationExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection collection, string rootPath)
     {
-        var envPath = Path.GetFullPath(Path.Combine(rootPath, "../.env"));
-        collection.AddDotEnv<DiscordSettings>(envPath);
-        collection.AddDotEnv<SteamSettings>(envPath);
-        collection.AddDotEnv<AppSettings>(envPath);
+        if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER")?.Equals("true", StringComparison.InvariantCultureIgnoreCase) is true)
+        {
+            new EnvLoader().AvoidModifyEnvironment().Load();
+            collection.AddDotEnv<DiscordSettings>();
+            collection.AddDotEnv<SteamSettings>();
+            collection.AddDotEnv<AppSettings>();
+        }
+        else
+        {
+            var envPath = Path.GetFullPath(Path.Combine(rootPath, "../.env"));
+
+            collection.AddDotEnv<DiscordSettings>(envPath);
+            collection.AddDotEnv<SteamSettings>(envPath);
+            collection.AddDotEnv<AppSettings>(envPath);
+        }
 
         collection.AddMediatR(options =>
         {
